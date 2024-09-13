47838a25db4975fee881cfc322787b4c24f9f002
Sachinjha08 <139585222+Sachinjha08@users.noreply.github.com>
2024-09-13 23:03:50 +0530

    Update Reservation.jsx

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -19,7 +19,7 @@ const Reservation = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://restaurent-backend-74iz.onrender.com', formData);
+            const response = await axios.post('https://restaurent-backend-74iz.onrender.com/api/v1/reservation', formData);
             toast.success('Reservation submitted successfully!');
             console.log('Reservation submitted:', response.data);
         } catch (error) {
